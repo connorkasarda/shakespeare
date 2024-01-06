aaf9a1908651d5d7587afd7e60ece62da302bf45
connorkasarda <connorkasarda@gmail.com>
2024-01-06 11:12:29 -0700

    initial creation of tokenizer files. type aliases for sentence and token. updated logs regarding tokenization approach.

---
+++ shakespeare/include/sentence.h
@@ -0,0 +1,14 @@
+#ifndef SENTENCE_H
+#define SENTENCE_H
+
+// author: connor kasarda
+// purpose: type alias for corpus sentences
+
+#include <string>
+
+namespace shakespeare
+{
+    using Sentence = std::string;
+}
+
+#endif
--- shakespeare/src/tokenizer.cpp
+++ shakespeare/src/tokenizer.cpp
@@ -2,5 +2,49 @@
 
 namespace shakespeare
 {
-    
+    Tokenizer::Tokenizer() = default;
+    void Tokenizer::Train
+    (
+        const std::vector<Sentence>& corpus,
+        size_t vocab_size
+    )
+    {
+        
+    }
+    std::vector<Token> Tokenizer::Tokenize(const Sentence& input)
+    {
+        return std::vector<Token>();
+    }
+    void Tokenizer::SaveModel(const std::string& filename)
+    {
+        
+    }
+    void Tokenizer::LoadModel(const std::string& filename)
+    {
+        
+    }
+    void Tokenizer::InitVocab(const std::vector<Sentence>& corpus)
+    {
+        
+    }
+    std::pair<Token, Token> FindMostFrequentPair
+    (
+        const std::vector<Sentence>& corpus
+    )
+    {
+        return std::make_pair(Token(), Token());
+    }
+    void UpdateVocab(const Token& new_token)
+    {
+        
+    }
+    void ReplaceMostFrequentPair
+    (
+        std::vector<Sentence>& corpus,
+        const std::pair<Token, Token>& pair,
+        const Token& new_token
+    )
+    {
+        
+    }
 }
